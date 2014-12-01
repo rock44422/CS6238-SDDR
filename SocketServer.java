8f448a2bfd3ffc2e913960bfb7d283a43fefafa6
Harsh Chawla <hchawla@localhost.localdomain>
2014-12-01 01:44:36 -0500

    Key addition and printing
    Signed-off-by: Harsh Chawla <hchawla@localhost.localdomain>

--- SocketServer.java
+++ SocketServer.java
@@ -1,7 +1,8 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import org.bouncycastle.*;
+import java.security.cert.*;
+import java.security.*;
 
 public class SocketServer {
     
@@ -24,37 +25,43 @@ public class SocketServer {
         	System.out.println("The following client has connected:"+client.getInetAddress().getCanonicalHostName());
         	//A client has connected to this server. Send welcome message
 		askClientInfo(client);
-		certExchange();
-            Thread thread = new Thread(new SocketClientHandler(client,readResponse(client)));
+		String client_Name = readResponse(client);
+		certExchange(client_Name);
+            Thread thread = new Thread(new SocketClientHandler(client,client_Name));
             thread.start();
         }     
     }
-    	public void certExchange()
+    	public void certExchange(String client_Name) throws IOException, InterruptedException
 	{
 		try
 		{
 		System.out.println("Client certificate requested and Server certificate sent");	
 		String s;
-		Process p = Runtime.getRuntime().exec("openssl verify -verbose -CAfile CA/ca.crt  Client-1/Client1.crt");
+		Process p = Runtime.getRuntime().exec("openssl verify -verbose -CAfile CA/ca.crt " + client_Name+"/"+client_Name+".crt");
             	BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
             	while ((s = br.readLine()) != null)
 		{
-			if(s.equals("Client-1/Client1.crt: OK"))
+			if(s.equals(client_Name+"/"+client_Name+".crt: OK"))
 			{
-				System.out.println("Client verification successfull");
+				System.out.println("Client verification successful");
 			}
 			else
 			{
 				System.out.println("Client verification failed");
+				System.exit(0);
 			}
 		}
 	        p.destroy();
+		FileInputStream fin = new FileInputStream(client_Name+"/"+client_Name+".crt");
+		CertificateFactory f = CertificateFactory.getInstance("X.509");
+		X509Certificate certificate = (X509Certificate)f.generateCertificate(fin);
+		PublicKey pk = certificate.getPublicKey();
+		System.out.println(pk);
        		 } catch (Exception e) {e.printStackTrace();}
 	}
         private void askClientInfo(Socket client) throws IOException, InterruptedException {
 	//ufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
 	//writer.write("Enter your CN");
-	//writer.flush();
 	//writer.close();
 	OutputStreamWriter writer = new OutputStreamWriter(client.getOutputStream());
 	writer.write("Enter your CN\n",0, "Enter your CN\n".length());
